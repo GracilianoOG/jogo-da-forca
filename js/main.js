5252af86d4efaf1115016baa396fe4bbd1b0cfbf
Gabriel <gabriel.gracilianomb@gmail.com>
2022-07-19 16:46:13 -0300

    Função para verificar as letras criada

--- js/main.js
+++ js/main.js
@@ -23,16 +23,15 @@ btnCancelar.addEventListener("click", funcCancelar);
 btnNovoJogo.addEventListener("click", funcNovoJogo);
 btnDesistir.addEventListener("click", funcDesistir);
 
+document.addEventListener("keydown", verificaLetra);
+
 // Tabuleiro
-var c = document.querySelector(".tabuleiro");
-var ctx = c.getContext("2d");
+var tela = document.querySelector(".tabuleiro");
+var pintura = tela.getContext("2d");
 var palavraSecreta = ["ALURA", "ORACLE", "JAVA", "JAVASCRIPT", "PYTHON", "SUN", "CAELUM", "HTML", "CSS"];
 
-function geraItemAleatorio(arr) {
-    return arr[Math.floor(Math.random() * arr.length)]
-}
-
-function geraLinhas(canvas, quantidadeLinhas) {
+// Tabuleiro - Funções
+function desenhaLinhas(canvas, quantidadeLinhas) {
     var tabuleiroLargura = canvas.width;
     var tabuleiroAltura = canvas.height;
     var espaco = (tabuleiroLargura / quantidadeLinhas) * 0.2; // O espaço entre as linhas corresponde a x porcento da linha em si
@@ -42,21 +41,33 @@ function geraLinhas(canvas, quantidadeLinhas) {
     var linhaPosX = espaco / 2;
     var linhaPosY = Math.round((tabuleiroAltura / 1.2) - (linhaAltura / 1.2)); // Coloca o meio da linha no meio do tabuleiro
 
-    ctx.clearRect(0, 0, tabuleiroLargura, tabuleiroAltura);
+    pintura.clearRect(0, 0, tabuleiroLargura, tabuleiroAltura);
 
     for(var i = 0; i < quantidadeLinhas; i++) {
-        ctx.fillStyle = "#0A3871";
-        ctx.fillRect(linhaPosX, linhaPosY, linhaLargura, linhaAltura);
+        pintura.fillStyle = "#0A3871";
+        pintura.fillRect(linhaPosX, linhaPosY, linhaLargura, linhaAltura);
 
         linhaPosX += linhaLargura + espaco;
     }
 }
 
+function geraItemAleatorio(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
 function geraPalavraAleatoria(arr) {
     var palavraAleatoria = geraItemAleatorio(arr);
     var palavraAleatoriaTamanho = palavraAleatoria.length;
-    geraLinhas(c, palavraAleatoriaTamanho);
+    desenhaLinhas(tela, palavraAleatoriaTamanho);
     console.log("Palavra: " + palavraAleatoria + " Tamanho: " + palavraAleatoriaTamanho);
+}
+
+function verificaLetra(evento) {
+    var codLetra = evento.which;
+
+    if(codLetra >= 65 && codLetra <= 90) {
+        console.log("Letra: " + evento.key + " Código: " + evento.which);
+    }
 }
 
 //Funções
@@ -65,7 +76,7 @@ function trocaDePagina(pagAntiga, pagNova) {
     pagNova.classList.remove("invisivel");
 }
 
-// Funções dos botões
+// Botões - Funções
 function funcComecaJogo() {
     trocaDePagina(pagPrincipal, pagJogo);
     // Gera palavra aleatória no tabuleiro()
